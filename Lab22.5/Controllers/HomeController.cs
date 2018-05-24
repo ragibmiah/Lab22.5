3bddc3a83f04d55fa9dbbc3f53215e0ab66d3dc2
Ragib Miah <ragib@umich.edu>
2018-05-24 09:34:45 -0400

    first commit

---
+++ Lab22.5/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab22._5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string userName, string color, string age)
+        {
+
+            ViewBag.UserName = userName;
+            ViewBag.Color = color;
+            ViewBag.Age = age;
+
+            Session["userName"] = ViewBag.UserName;
+            Session["color"] = ViewBag.Color;
+            Session["age"] = ViewBag.Age;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab22.5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab22._5.MvcApplication" Language="C#" %>
